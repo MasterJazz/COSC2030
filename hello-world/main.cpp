35af3d19d19315330ebedba1793c9b0d01692935
Matthew <matthewsleight@westernwyoming.edu>
2025-01-24 18:09:54 +0000

    Updating/Syncing new fork

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,14 @@
 //
-// Name
-// Date
-// Hello World Programming Project
+// Matthew Sleight
+// January 13, 2025
+// Your First C++ Program
 // COSC 2030
 //
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
