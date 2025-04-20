d87b28fe73dfbca7141c30a7b78cbd07385c1145
Matthew <matthewsleight@westernwyoming.edu>
2025-04-20 21:28:03 +0000

    Ready for grading

--- recursive-multiplication/main.cpp
+++ recursive-multiplication/main.cpp
@@ -1,7 +1,33 @@
 //
-// Name
-// Date
+// Matthew Sleight
+// April 20 2025
 // Recursive Multiplication Programming Project
 // COSC 2030
 //
 
+#include <iostream>
+
+using namespace std;
+
+// Function prototype
+int multiply(int, int);
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter 2 numbers and I will multiply them: ";
+    cin >> num1 >> num2;
+    cout << "Their product is " << multiply(num1, num2) << endl;
+
+    return 0;
+}
+
+int multiply(int x, int y) {
+    if (x == 0 || y == 0) {
+        return 0;
+    } else if (y == 1) {
+        return x;
+    } else {
+        return x + multiply(x, y-1);
+    }
+}
